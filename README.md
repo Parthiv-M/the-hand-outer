43d1bb4156e6f03e358ca77d75f457483d682104
Parthiv Menon <parthivmenon01@gmail.com>
2021-07-03 10:38:56 +0530

    hand-outer code completion

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# the-hand-outer
+# The Hand-Outer
+
+A _hand-outer_ application that does what it claims—hand out newsletters in this case. This hand-outer doles out a copy of the newsletter to every subscriber's email address (which is fetched from the database via an API) every week, scheduled by [node-cron](https://www.npmjs.com/package/node-cron) and powered by [nodemailer](https://www.npmjs.com/package/nodemailer).
+
+## A general use-case
+
+This project can be used in a general scenario as well. Instead of being limited to just sending e-mails, it can be configured to repeat menial tasks automatically at specified intervals of time. Replacing parts of the code with your own to-be-repeated operation will do the trick. In this specific case, I have leveraged [express](https://expressjs.com/) to make use of a few of its functionalities. But even that can be avoided, if required, since a normal script involving node-cron can also be used in several other specific use-cases.
+
+## Points to note for HTML in email
+
+I stumbled quite a bit while setting this up for myself, and want to note down a few points for general consideration. 
+
+- While setting up scheduled e-mailers for a variety of content, it is best to use some form of server side templating engine if you want dynamic code injection, since executing a script before sending across the html might prove to be quite a task.
+- `<table>` elements are the safest when it comes to setting up HTML for emails. The following block of code should get you started.
+  ```
+  <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse: collapse;">
+    <td align="center" border="0" bgcolor="white">
+      <! –– optionally, headings can be placed here -->
+      <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+        <td align="center" bgcolor="white" border="0">
+          <! –– place your content here -->
+        </td>
+      </table> 
+    </td>
+  </table>
+  ```
+- Using a lot of `<div>` elements is generally to be avoided, even though I did not face any major issue.
+- Support for Cascading Style Sheets (CSS) is limited for HTML in emails. Hence, inline styling is the way forward. 
+  - Special note: Gmail **does not** render your custom font faces. 
+- The standard width if you are looking for impressive looking HTML in email is `600px`. It scales well on mobile devices as well.
+  - It should be mentioned as `width="600"` and not `width="600px"` 
+- Another issue I faced was sending mails to Outlook. Outlook only accepts `text` emails—meaning `html` templates are not accepted.
+
+Generally, it is challenging to figure out the perfect styling for your HTML in emails. But, a bit of searching and experimenting can get you the look you require.
---
+++ server.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
+const axios = require('axios');
+const hbs = require('nodemailer-express-handlebars');
+
+// load environment variables
+require('dotenv').config();
+
+const app = express();
+
+// set the view engine
+app.set('view engine', 'handlebars');
+
+// define the port
+const port = process.env.PORT || 3000;
+
+// getting all the environment variables
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+const serverUrl = process.env.SERVER_URL;
+const emailHost = process.env.EMAIL_HOST;
+const clientToken = process.env.CLIENT_TOKEN;
+
+// custom constants
+const subject = 'CyberManipal | The Tech News You Need';
+let mailingList = "";
+
+// custom list of greetings to pick randomly from
+const greetingList = [
+    "Here's your weekly dose of byte sized cybersecurity news.",
+    "Wanna hear about the latest in tech? Read it on CyberManipal.",
+    "(sudo apt-get) update yourself with this handout of cybersecurity news.",
+    "If you are looking for tech news, look no further.",
+    "Glance through curated, informative news from the realms of technology."
+]
+
+// schedule the cron to hand out newsletters every thursday
+cron.schedule('0 0 * * 4', async () => {
+    try {
+        // fetch the latest news to be hadned out
+        const resNews = await axios.get(`${serverUrl}/api/news?page=0`);
+        let latestFive = resNews.data.data.slice(0, 5);
+
+        // format the date field and the link field
+        latestFive.forEach(news => {
+            news.link = `https://wearemist.in/news/article/${news._id}`
+            let tempDate = new Date(news.date);
+            news.date = tempDate.toDateString();
+        })
+        
+        // fetch the subscribers
+        const resEmails = await axios.get(`${serverUrl}/api/newsletter`, {
+            headers: {
+                "client_token" : clientToken
+            }
+        });
+        subscriberList = resEmails.data.data;
+        
+        // create the mailing list
+        let tempList = [];
+        subscriberList.forEach(sub => {
+            tempList.push(sub.email)
+        });
+        mailingList = tempList.join(", ");
+    
+        // create a transporter object
+        const transporter = nodemailer.createTransport({
+            host: emailHost, // configure to use your mail host
+            port: 465,
+            secure: true, // use SSL
+            auth: {
+                user: email,
+                pass: password,
+            },
+        });
+
+        // transporter options for hbs
+        var options = {
+            viewEngine : {
+                extname: '.hbs', // handlebars extension
+                layoutsDir: 'views/email/', // location of handlebars templates
+                defaultLayout: 'index', // name of main template
+                partialsDir: __dirname + 'views/email/partials/'
+            },
+            viewPath: 'views/email',
+            extName: '.hbs'
+        }
+
+        // apply hbs config
+        transporter.use('compile', hbs(options));
+
+        // defining all the email options
+        const messageOptions = {
+            from: email,
+            to: mailingList,
+            subject: subject,
+            template: 'index',
+            context: {
+                news: latestFive,
+                greeting: greetingList[Math.floor(Math.random() * greetingList.length)]
+            }
+        }
+
+        // send the email
+        transporter.sendMail(messageOptions, () => {
+            // optional callback here
+        });
+
+        console.log('Emails for ' + new Date().toISOString().slice(0, 10) + ' sent successfully');
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+// buffer route
+app.get("*", (req, res) => {
+    res.status(200).send("<h4 style='font-family: sans-serif'>Wait for next Thursday for your newsletter</h4>");
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
